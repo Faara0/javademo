1fb2ec3783c9bf4b1c2f6ee16d46bbd056bd2fee
Faara0 <toivovanhasaari@gmail.com>
2024-01-15 15:21:57 +0200

    message

---
+++ src/test/java/main/App.java
@@ -0,0 +1,11 @@
+package main;
+
+public class App {
+    public static void main( String[] args) 
+    {
+        System.out.println("Hello world!" );
+        System.out.print("Tekstiä ");
+        System.out.println("tänne");
+        System.out.println(666);
+    }
+}
